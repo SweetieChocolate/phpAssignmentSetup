11ed79512853686696f56bb792af46d1d68559a4
SweetieChocolate <79972040+SweetieChocolate@users.noreply.github.com>
2023-01-30 23:56:44 +0700

    .

--- index.php
+++ index.php
@@ -4,9 +4,17 @@ echo session_id() . '<br>';
 ?>
 <html>
     <head>
-
+        <script>
+            function changeFrame(item) {
+                var frame = document.getElementById("frameView");
+                frame.setAttribute("src", item.getAttribute("url"));
+                frame.reload();
+            }
+        </script>
     </head>
     <body>
-        <iframe src="test.php" frameborder="0" width="100%" height="100%"></iframe>
+        <button type="button" url="test.php" onclick="changeFrame(this)">test</button>
+        <button type="button" url="test2.php" onclick="changeFrame(this)">test2</button>
+        <iframe id ="frameView" src="test.php" frameborder="0" width="100%" height="100%"></iframe>
     </body>
 </html>
--- test2.php
+++ test2.php
@@ -3,7 +3,7 @@
 
 </head>
 <body>
-    <b>this is a test</b>
+    <b>this is another test version 2</b>
     <gridview id="gvDetails" TableName="Employment">
         <column PropertyName="Person->FullName" HeaderText="Full Name"></column>
         <column PropertyName="Person->FamilyName" HeaderText="Last Name"></column>
