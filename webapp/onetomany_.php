b33d72550430684f3c375f75750234d440a2fe8d
SweetieChocolata <121809353+undertheland@users.noreply.github.com>
2023-03-01 22:26:05 +0700

    .

--- webapp/onetomany_.php
+++ webapp/onetomany_.php
@@ -63,13 +63,9 @@
                                         </td>
                                     </tr>
                                     <!-- Modal -->
-                                    <div class="modal fade" id="exampleModal<?= $data['code'] ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                    <div class="modal fade" id="exampleModal<?= $data['code'] ?>" tabindex="-1">
                                         <div class="modal-dialog">
                                             <div class="modal-content">
-                                                <div class="modal-header">
-                                                    <h1 class="modal-title fs-5" id="exampleModalLabel">Edit Book Form</h1>
-                                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                                </div>
                                                 <form action="action.php" method="post">
                                                     <div class="modal-body">
                                                         <label>Code: </label>
